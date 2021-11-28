3099b3c8431eb4df31714614a829b9adfab1e46c
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-11-27 20:18:09 -0800

    fixed seed

--- db/seed.sql
+++ db/seed.sql
@@ -4,23 +4,23 @@ VALUES  ("Engineering"),
         ("Legal"),
         ("Sales");
 
-INSERT INTO  role (id,title,salary,department_id)
-VALUES  (1,"sales lead", 150000,4),
-        (2,"Salesperson", 80000,4),
-        (3,"Lead Engineer", 140000,1),
-        (4,"Software Engineer", 120000,1),
-        (5,"Account Manager", 140000,2),
-        (6,"Accountant", 125000,2),
-        (7,"Legal Team Lead", 130000,3),
-        (8,"Lawyer", 130000,3);
+INSERT INTO  role (title,salary,department_id)
+VALUES  ("sales lead", 150000,4),
+        ("Salesperson", 80000,4),
+        ("Lead Engineer", 140000,1),
+        ("Software Engineer", 120000,1),
+        ("Account Manager", 140000,2),
+        ("Accountant", 125000,2),
+        ("Legal Team Lead", 130000,3),
+        ("Lawyer", 130000,3);
 
 
-INSERT INTO employee (id,first_name,last_name,role_id,manager_id)
-VALUES  (3,"John", "Doe",3, NULL ),
-        (1,"Mike", "Chan",1,3),
-        (2,"Ashley", "Rodriguez",2,3),
-        (4,"Kevin", "Tupik",4,NULL),
-        (5,"Kunal", "Singh",5,7),
-        (6,"Malia", "Brown",6,7),
-        (7,"Sarah", "Lourd",7,NULL),
-        (8,"Tom", "Allen",8,4);
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VALUES  ("John", "Doe",3, NULL ),
+        ("Mike", "Chan",1,NULL),
+        ("Ashley", "Rodriguez",2,2),
+        ("Kevin", "Tupik",4,NULL),
+        ("Kunal", "Singh",5,NULL),
+        ("Malia", "Brown",6,3),
+        ("Sarah", "Lourd",7,NULL),
+        ("Tom", "Allen",8,4);
